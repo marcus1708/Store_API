9c3d950309b8e5b5dafa870b421378ac6bd68a7d
marcus1708 <marcusviniciusbordin@gmail.com>
2026-05-16 17:47:59 -0300

    swagger

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
+require('dotenv').config();
+
 const express = require('express');
-const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocs = require('./config/swagger');
+const YAML = require('yamljs');
+
 const routes = require('./routes');
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
-// Rota da documentação
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+/* ---------------- SWAGGER ---------------- */
+
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+/* ---------------- ROTAS ---------------- */
 
 app.use(routes);
 
